29689b5da32a11903dca8ef6830c69bf0c11652e
Qmeriaux <qmeriaux@orange.fr>
2004-12-12 07:53:03 +0100

    ouais la zone

--- app/src/main/java/com/meriaux/ecommerce/MainActivity.kt
+++ app/src/main/java/com/meriaux/ecommerce/MainActivity.kt
@@ -38,6 +38,7 @@ class MainActivity : AppCompatActivity() {
                 productList.add(product)
                 //notify adapter
                 productsAdapter.notifyDataSetChanged()
+
             }
         }
 
